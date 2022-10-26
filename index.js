69fc6b0cb8121fe7420c789e3340b0f42da54bc6
Nick Stevens <90112060+stezzzy@users.noreply.github.com>
2022-10-26 19:28:23 -0400

    set up db files, created function to connect to mysql

--- index.js
+++ index.js
@@ -11,3 +11,35 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employee_tracker_db database.`)
 );
+
+db.connect((err) => {
+    if (err) throw err;
+  });
+  function init(){
+    inquirer.prompt([
+      {
+          type: "list",
+          message: "What would you like to do?", 
+          choices: ["View All Employees", "Add Employees", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"],
+          name: "choices"
+      },
+    ]).then(welcome => {
+      if(welcome.choices === "View All Employees"){
+          viewAllEmployees()
+      } else if (welcome.choices === "Add Employees"){
+          addEmployees()
+      } else if (welcome.choices === "Update Employee Role"){
+          updateEmployeeRole()
+      } else if (welcome.choices === "View All Roles"){
+          viewAllRoles()
+      } else if (welcome.choices === "Add Role"){
+          addRole()
+      } else if (welcome.choices === "View All Departments"){
+          viewAllDepartments()
+      } else if(welcome.choices === "Add Department"){
+          addDepartment()
+      } else {
+          process.exit()
+      }
+  });
+  };
