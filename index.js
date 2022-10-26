39d5bc1a69402602fb5c0dc6169ed085a0d001dc
Nick Stevens <90112060+stezzzy@users.noreply.github.com>
2022-10-26 19:31:03 -0400

    built out rest of index logic to CRUD departments, employees, roles

--- index.js
+++ index.js
@@ -43,3 +43,115 @@ db.connect((err) => {
       }
   });
   };
+
+  init()
+
+function viewAllDepartments(){
+    db.query(`SELECT * from department`, (err, results) => {
+        console.table(results)
+        init()
+    });
+};
+
+function viewAllEmployees(){
+    db.query(`SELECT * from employee`, (err, results) => {
+        console.table(results)
+        init()
+    });
+};
+
+function viewAllRoles(){
+    db.query(`SELECT * from role`, (err, results) => {
+        console.table(results)
+        init()
+    });
+};
+
+function addEmployees(){
+    db.query(`SELECT * from role`, (err, results) => {
+        console.table(results)
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the employee's first name?", 
+            name: "first_name"
+        },
+        {
+            type: "input",
+            message: "What is the employee's last name?", 
+            name: "last_name"
+        },
+        {
+            type: "list",
+            message: "Who is the employee's manager id?", 
+            name: "manager_id",
+            choices: [1, 2, "null", 4]
+        },
+        {
+            type: "list",
+            message: "What is the employee's role id?", 
+            name: "role_id",
+            choices: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        },
+
+    ]).then(function(answers){
+        console.log(answers);
+        
+        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answers.first_name}", "${answers.last_name}", ${answers.role_id}, ${answers.manager_id})`,
+        function(error) {
+            if (error) throw error;
+            console.log("Added Employee")
+            init()
+        })
+    })
+})
+}
+
+function addDepartment(){
+    inquirer.prompt({
+        type: "input",
+        message: "What is the departments name?", 
+        name: "department"
+    }).then(hotdogs => {
+        db.query(`INSERT INTO department (department) VALUES ("${hotdogs.department}")`,
+        function(error) {
+            if (error) throw error;
+            console.log("Added hotdog")
+            init()
+        })
+    })
+
+}
+
+function addRole(){
+    db.query(`SELECT * from department`, (err, results) => {
+        console.table(results)
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the role's title?", 
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is the salary?", 
+            name: "salary"
+        },
+        {
+            type: "list",
+            message: "Who is the department id?", 
+            name: "department_id",
+            choices: [1, 2, 3, 4, 5]
+        },
+    ]).then(function(answers){
+        console.log(answers);
+        
+        db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${answers.title}", "${answers.salary}", ${answers.department_id})`,
+        function(error) {
+            if (error) throw error;
+            console.log("Added Role")
+            init()
+        })
+    })
+})
+}
